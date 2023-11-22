7e5d68d697ca79eb563d1a44f0263e6aecb8769e
danmv24 <{{N:dX"`zsFfoZ9>
2023-11-22 18:23:06 +0300

    change

--- src/main/java/com/example/FinancialTracker/enums/TransactionType.java
+++ src/main/java/com/example/FinancialTracker/enums/TransactionType.java
@@ -2,10 +2,7 @@ package com.example.FinancialTracker.enums;
 
 import lombok.AllArgsConstructor;
 
-@AllArgsConstructor
 public enum TransactionType {
-    INCOME("INCOME"),
-    EXPENSE("EXPENSE");
-
-    private String value;
+    INCOME,
+    EXPENSE;
 }
