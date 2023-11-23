c2ee1ea50ee8b8f20c24c319d9f43cd604bce38e
danmv24 <{{N:dX"`zsFfoZ9>
2023-11-23 22:25:33 +0300

    repository for categories and new method findByCategoryName

---
+++ src/main/java/com/example/FinancialTracker/repository/CategoryRepository.java
@@ -0,0 +1,14 @@
+package com.example.FinancialTracker.repository;
+
+import com.example.FinancialTracker.entity.CategoryEntity;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CategoryRepository extends CrudRepository<CategoryEntity, Long> {
+
+    Optional<CategoryEntity> findByCategoryName(String categoryName);
+
+}
