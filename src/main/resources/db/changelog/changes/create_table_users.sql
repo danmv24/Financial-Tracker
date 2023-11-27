3d0053377ac405a6dcc56cd1c41d0441a2e886f1
danmv24 <{{N:dX"`zsFfoZ9>
2023-11-27 20:07:36 +0300

    create table users

---
+++ src/main/resources/db/changelog/changes/create_table_users.sql
@@ -0,0 +1,7 @@
+CREATE TABLE users(
+    id BIGSERIAL PRIMARY KEY,
+    username VARCHAR(64) UNIQUE NOT NULL,
+    name VARCHAR(64) NOT NULL,
+    surname VARCHAR(64) NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
