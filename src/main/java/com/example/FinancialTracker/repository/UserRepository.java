d9ce481d746802b164193035eb9ca783ba298736
danmv24 <{{N:dX"`zsFfoZ9>
2023-11-27 20:12:34 +0300

    created method for user search

--- src/main/java/com/example/FinancialTracker/repository/UserRepository.java
+++ src/main/java/com/example/FinancialTracker/repository/UserRepository.java
@@ -4,6 +4,11 @@ import com.example.FinancialTracker.entity.UserEntity;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends CrudRepository<UserEntity, Long> {
+
+    Optional<UserEntity> findByUsername(String username);
+
 }
