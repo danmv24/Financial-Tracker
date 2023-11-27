12986f52555a58430fe358a87b8c41d9600e6f09
danmv24 <{{N:dX"`zsFfoZ9>
2023-11-27 23:01:36 +0300

    default user details service

---
+++ src/main/java/com/example/FinancialTracker/service/impl/DefaultUserDetailsService.java
@@ -0,0 +1,24 @@
+package com.example.FinancialTracker.service.impl;
+
+import com.example.FinancialTracker.entity.UserEntity;
+import com.example.FinancialTracker.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class DefaultUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserEntity user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User with username " +username+" not found!"));
+
+        return new DefaultUserDetails(user);
+    }
+}
