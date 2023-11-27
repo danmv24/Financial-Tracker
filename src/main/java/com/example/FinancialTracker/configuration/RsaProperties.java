f6f8edb9dd483f727d77d289a436d79cb41c125f
danmv24 <{{N:dX"`zsFfoZ9>
2023-11-27 20:32:47 +0300

    class for accessing rsa keys

---
+++ src/main/java/com/example/FinancialTracker/configuration/RsaProperties.java
@@ -0,0 +1,20 @@
+package com.example.FinancialTracker.configuration;
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+
+@ConfigurationProperties(prefix = "rsa")
+@Getter
+public class RsaProperties {
+
+    @Value("${rsa.private-key}")
+    private RSAPrivateKey privateKey;
+
+    @Value("${rsa.public-key}")
+    private RSAPublicKey publicKey;
+
+}
