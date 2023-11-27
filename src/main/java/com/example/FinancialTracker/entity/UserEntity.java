c7925e838d9d2356c298b9eb792f8e8a589a7039
danmv24 <{{N:dX"`zsFfoZ9>
2023-11-27 20:00:31 +0300

    create user entity

---
+++ src/main/java/com/example/FinancialTracker/entity/UserEntity.java
@@ -0,0 +1,30 @@
+package com.example.FinancialTracker.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Entity(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "username",unique = true)
+    @NotNull
+    private String username;
+
+    @NotNull
+    private String name;
+
+    @NotNull
+    private String surname;
+
+}
