8536513c8edbe6048ef867f2594619e7513cf702
danmv24 <{{N:dX"`zsFfoZ9>
2023-11-28 18:07:57 +0300

    method exists by username

--- src/main/java/com/example/FinancialTracker/repository/UserRepository.java
+++ src/main/java/com/example/FinancialTracker/repository/UserRepository.java
@@ -11,4 +11,6 @@ public interface UserRepository extends CrudRepository<UserEntity, Long> {
 
     Optional<UserEntity> findByUsername(String username);
 
+    boolean existsByUsername(String username);
+
 }
