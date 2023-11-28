b55d63e22fd0fcc75e25f4a8a68b45ba312a08d1
danmv24 <{{N:dX"`zsFfoZ9>
2023-11-28 18:09:55 +0300

    sign up

---
+++ src/main/java/com/example/FinancialTracker/controller/AuthController.java
@@ -0,0 +1,22 @@
+package com.example.FinancialTracker.controller;
+
+import com.example.FinancialTracker.form.UserForm;
+import com.example.FinancialTracker.service.AuthService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/signup")
+    public void signUp(@RequestPart("user") UserForm userForm) {
+        authService.create(userForm);
+    }
+}
