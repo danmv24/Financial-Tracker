cdbfde79b00e2842058237e5941d5932d87f97b2
danmv24 <{{N:dX"`zsFfoZ9>
2023-11-30 18:30:49 +0300

    changes

--- src/main/java/com/example/FinancialTracker/service/TransactionService.java
+++ src/main/java/com/example/FinancialTracker/service/TransactionService.java
@@ -3,12 +3,13 @@ package com.example.FinancialTracker.service;
 import com.example.FinancialTracker.form.TransactionForm;
 import com.example.FinancialTracker.view.TransactionView;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 import java.util.Map;
 
 public interface TransactionService {
 
-    void addTransaction(TransactionForm transactionForm);
+    void addTransaction(TransactionForm transactionForm, HttpServletRequest request);
 
     List<TransactionView> allTransactions();
 
