d76e1d42256a0a817267622c55cfc8e530edf9a5
danmv24 <{{N:dX"`zsFfoZ9>
2023-12-14 19:48:31 +0300

    the logic of adding a transaction to the database has been redesigned. the category Name field has been added when displaying all user transactions on the screen.

--- src/main/java/com/example/FinancialTracker/service/impl/DefaultTransactionService.java
+++ src/main/java/com/example/FinancialTracker/service/impl/DefaultTransactionService.java
@@ -1,11 +1,8 @@
 package com.example.FinancialTracker.service.impl;
 
-import com.example.FinancialTracker.entity.CategoryEntity;
 import com.example.FinancialTracker.entity.TransactionEntity;
-import com.example.FinancialTracker.entity.UserEntity;
 import com.example.FinancialTracker.exception.TransactionException;
 import com.example.FinancialTracker.form.TransactionForm;
-import com.example.FinancialTracker.mapper.CategoryMapper;
 import com.example.FinancialTracker.mapper.TransactionMapper;
 import com.example.FinancialTracker.repository.CategoryRepository;
 import com.example.FinancialTracker.repository.TransactionRepository;
@@ -19,7 +16,6 @@ import org.springframework.stereotype.Service;
 import javax.servlet.http.HttpServletRequest;
 import java.math.BigDecimal;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -34,41 +30,41 @@ public class DefaultTransactionService implements TransactionService {
 
     @Override
     public void addTransaction(TransactionForm transactionForm, HttpServletRequest request) {
-        UserEntity user = tokenService.parseToken(request);
+        Long userId = tokenService.parseToken(request);
 
         if (transactionForm.getAmount().compareTo(BigDecimal.ZERO) == 0) {
             throw new TransactionException(HttpStatus.BAD_REQUEST, "Amount cannot be zero");
         }
 
-        Optional<CategoryEntity> categoryOptional = categoryRepository.findByCategoryName(transactionForm.getCategoryName());
+        Long categoryId = categoryRepository.findByCategoryNameIgnoreCase(transactionForm.getCategoryName()).orElseThrow(
+                () -> new TransactionException(HttpStatus.NOT_FOUND, "The category with the name " + transactionForm.getCategoryName() + " was not found!")).getId();
 
-        if (categoryOptional.isPresent()) {
-            transactionRepository.save(TransactionMapper.toEntity(transactionForm, categoryOptional.get(), user));
-        } else {
-            CategoryEntity category = categoryRepository.save(CategoryMapper.toEntity(transactionForm));
-            transactionRepository.save(TransactionMapper.toEntity(transactionForm, category, user));
-        }
+        transactionRepository.save(TransactionMapper.toEntity(transactionForm, categoryId, userId));
     }
 
     @Override
     public List<TransactionView> allTransactions(HttpServletRequest request) {
-        UserEntity user = tokenService.parseToken(request);
+        Long userId = tokenService.parseToken(request);
 
-        List<TransactionEntity> transactions = transactionRepository.findAllByUserId(user.getId());
+        List<TransactionEntity> transactions = transactionRepository.findAllByUserIdOrderByDateDesc(userId);
 
         return transactions.stream()
-                .map(TransactionMapper::toView)
-                .collect(Collectors.toList());
+                .map(transaction -> {
+                    TransactionView transactionView = TransactionMapper.toView(transaction);
+                    categoryRepository.findById(transaction.getCategoryId())
+                            .ifPresent(category -> transactionView.setCategory(category.getCategoryName()));
+                    return transactionView;
+                }).collect(Collectors.toList());
     }
 
     @Override
     public void delete(Long id, HttpServletRequest request) {
-        UserEntity user = tokenService.parseToken(request);
+        Long userId = tokenService.parseToken(request);
 
         TransactionEntity transaction = transactionRepository.findById(id).orElseThrow(
                 () -> new TransactionException(HttpStatus.NOT_FOUND, "Transaction with id " + id + " does not exist"));
 
-        if (!user.getId().equals(transaction.getUser().getId())) {
+        if (!userId.equals(transaction.getUserId())) {
             throw new TransactionException(HttpStatus.FORBIDDEN, "User does not have permission to delete this transaction");
         }
 
