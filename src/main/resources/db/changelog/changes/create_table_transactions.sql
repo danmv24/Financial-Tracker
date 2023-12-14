cbbb5af44ab3aa57a7ce53b81c80647215821ecc
danmv24 <{{N:dX"`zsFfoZ9>
2023-12-14 19:44:21 +0300

    changes

--- src/main/resources/db/changelog/changes/create_table_transactions.sql
+++ src/main/resources/db/changelog/changes/create_table_transactions.sql
@@ -1,6 +1,9 @@
 CREATE TABLE transactions(
     transaction_id BIGSERIAL PRIMARY KEY,
     transaction_type VARCHAR(15) NOT NULL,
+    category_id BIGINT NOT NULL,
+    user_id BIGINT NOT NULL,
     amount DECIMAL(10, 2) NOT NULL,
+    comment VARCHAR(150) NOT NULL,
     date DATE NOT NULL
 );
