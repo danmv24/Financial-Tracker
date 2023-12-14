ca41e6a146ce28646b37573411cd38d7804134bc
danmv24 <{{N:dX"`zsFfoZ9>
2023-12-14 20:04:26 +0300

    deleted getAllTransactionsByType

--- src/main/java/com/example/FinancialTracker/controller/TransactionController.java
+++ src/main/java/com/example/FinancialTracker/controller/TransactionController.java
@@ -1,6 +1,5 @@
 package com.example.FinancialTracker.controller;
 
-import com.example.FinancialTracker.enums.TransactionType;
 import com.example.FinancialTracker.form.TransactionForm;
 import com.example.FinancialTracker.service.TransactionService;
 import com.example.FinancialTracker.view.TransactionView;
@@ -9,7 +8,6 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @AllArgsConstructor
@@ -26,11 +24,6 @@ public class TransactionController {
     @GetMapping("/all")
     public List<TransactionView> getAllTransactions(HttpServletRequest request) {
         return transactionService.allTransactions(request);
-    }
-
-    @GetMapping("/all/type")
-    public Map<TransactionType, List<TransactionView>> getAllTransactionsByType(HttpServletRequest request) {
-        return transactionService.allTransactionsByType(request);
     }
 
     @DeleteMapping("/delete/{id}")
