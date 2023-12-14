70e4bed20881a8eb5b2c893d7cf4092e6ac5a66b
danmv24 <{{N:dX"`zsFfoZ9>
2023-12-14 19:52:19 +0300

    changes

--- src/main/java/com/example/FinancialTracker/mapper/TransactionMapper.java
+++ src/main/java/com/example/FinancialTracker/mapper/TransactionMapper.java
@@ -1,8 +1,6 @@
 package com.example.FinancialTracker.mapper;
 
-import com.example.FinancialTracker.entity.CategoryEntity;
 import com.example.FinancialTracker.entity.TransactionEntity;
-import com.example.FinancialTracker.entity.UserEntity;
 import com.example.FinancialTracker.enums.TransactionType;
 import com.example.FinancialTracker.form.TransactionForm;
 import com.example.FinancialTracker.view.TransactionView;
@@ -12,11 +10,11 @@ import java.time.LocalDate;
 
 public class TransactionMapper {
 
-    public static TransactionEntity toEntity(TransactionForm transactionForm, CategoryEntity category, UserEntity user) {
+    public static TransactionEntity toEntity(TransactionForm transactionForm, Long categoryId, Long userId) {
         return TransactionEntity.builder()
                 .transactionType(transactionForm.getAmount().compareTo(BigDecimal.ZERO) > 0 ? TransactionType.INCOME : TransactionType.EXPENSE)
-                .category(category)
-                .user(user)
+                .categoryId(categoryId)
+                .userId(userId)
                 .amount(transactionForm.getAmount().abs())
                 .date(LocalDate.parse(transactionForm.getDate()))
                 .comment(transactionForm.getComment())
@@ -25,7 +23,6 @@ public class TransactionMapper {
 
     public static TransactionView toView(TransactionEntity transactionEntity) {
         return TransactionView.builder()
-                .id(transactionEntity.getId())
                 .transactionType(transactionEntity.getTransactionType())
                 .amount(transactionEntity.getAmount())
                 .date(transactionEntity.getDate().toString())
